9a19d33fddb7d8a81e2fa1c07a7cdd2ca2855338
Cory Clemens <44911328+coryclemens@users.noreply.github.com>
2018-11-13 17:34:44 -0800

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 camOffset;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + camOffset;
+
+
+     
+
+    }
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform trn;
+    public float rot = 1;
+    public float rotDeg;
+    public float rotSpeed = 1;
+    public float x;
+    public float z;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        trn = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Mathf.Cos(rotDeg * Mathf.PI / 180);
+        z = Mathf.Sin(rotDeg * Mathf.PI / 180);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddTorque(x, 0, z, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddTorque(-x, 0, -z, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotDeg += rotSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotDeg -= rotSpeed;
+
+        }
+    }
+}
